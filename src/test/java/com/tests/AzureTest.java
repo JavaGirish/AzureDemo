0626a84f8bc781b8eb256f9a4822cdfbd2964974
Girish Nair <45124213+JavaGirish@users.noreply.github.com>
2020-10-18 15:51:39 +0530

    updated test class

--- src/test/java/com/tests/AzureTest.java
+++ src/test/java/com/tests/AzureTest.java
@@ -27,8 +27,16 @@ public class AzureTest {
 	@BeforeMethod
 	public void setUp() {
 		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized"); // open Browser in maximized mode
+		options.addArguments("disable-infobars"); // disabling infobars
+		options.addArguments("--disable-extensions"); // disabling extensions
+		options.addArguments("--disable-gpu"); // applicable to windows os only
+		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+		options.addArguments("--no-sandbox"); // Bypass OS security model
+
+		driver = new ChromeDriver(options);
+		//driver.manage().window().maximize();
 		driver.get("https://www.tripadvisor.in/");
 
 		driver.manage().timeouts().pageLoadTimeout(3, TimeUnit.MINUTES);
