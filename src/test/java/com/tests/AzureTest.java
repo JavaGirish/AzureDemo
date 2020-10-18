3d8b054dfb5243ec0e91f6ff4128eab625824131
Girish Nair <45124213+JavaGirish@users.noreply.github.com>
2020-10-18 16:05:04 +0530

    Update AzureTest.java

--- src/test/java/com/tests/AzureTest.java
+++ src/test/java/com/tests/AzureTest.java
@@ -1,7 +1,7 @@
 package com.tests;
 
 import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.chrome;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
